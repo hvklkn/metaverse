f3c9aee2a81a8595ec44a810ca3e0bbaeb98d567
hvklkn <hvklkn@icloud.com>
2024-12-24 23:04:11 +0300

    susuzluk
    susuzluk durumu atandı

--- nesneye_yonelik_proje/meta.cpp
+++ nesneye_yonelik_proje/meta.cpp
@@ -37,45 +37,6 @@ public:
     void durumGoster();
 };
 
-#include <iostream>
-#include <string>
-#include <chrono>
-#include <thread>
-#include <stdexcept>
-#include <limits>
-
-using namespace std;
-
-class karakter {
-private:
-    int kimlikid;
-public:
-    float para;
-    float aclik;
-    float susuzluk;
-    bool uyku;
-    float guc;
-    int x, y;
-
-    void getpara(float para) { this->para = para; };
-    void getaclik(float aclik) { this->aclik = aclik; };
-    void getsusuzluk(float susuzluk) { this->susuzluk = susuzluk; };
-    void getguc(float guc) { this->guc = guc; };
-
-    float setpara() { return para; };
-    float setaclik() { return aclik; };
-    float setsusuzluk() { return susuzluk; };
-    float setguc() { return guc; };
-
-    void aclikdurumu();
-    void susuzlukdurumu();
-    void yemekYedir();
-    void suIc();
-    void hareket(int x, int y);
-    void hareket(float distance);
-    void durumGoster();
-};
-
 void karakter::aclikdurumu() {
     while (true) {
         aclik -= 5;
