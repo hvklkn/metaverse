f83633c3a911a428528a1986a6617ddae65ffae2
hvklkn <hvklkn@icloud.com>
2024-12-31 20:12:21 +0300

    Update meta.cpp
    durum fonksiyonu eklendi

--- nesneye_yonelik_proje/meta.cpp
+++ nesneye_yonelik_proje/meta.cpp
@@ -59,3 +59,57 @@ void karakter::susuzlukdurumu() {
     }
 }
 
+void karakter::yemekYedir() {
+    cout << "Karakter yemek yedi. Açlık sorunu giderildi." << endl;
+    aclik = 100;
+}
+
+void karakter::suIc() {
+    cout << "Karakter su içti. Susuzluk sorunu giderildi." << endl;
+    susuzluk = 100;
+}
+
+void guckarsilastirma(float guc1, float guc2) {
+    if (guc1 == guc2) {
+        cout << "Karakterlerin güçleri birbirine eşittir." << endl;
+    }
+    else if (guc1 > guc2) {
+        cout << "Karakter1 daha güçlüdür." << endl;
+    }
+    else {
+        cout << "Karakter2 daha güçlüdür." << endl;
+    }
+}
+
+void karakter::hareket(int x, int y) {
+    try {
+        int yon;
+        cout << "Hangi yöne hareket etsin? 0-SOL 1-SAG 2-YUKARI 3-ASAGI: ";
+        cin >> yon;
+
+        switch (yon) {
+            case 0: x -= 1; break;
+            case 1: x += 1; break;
+            case 2: y += 1; break;
+            case 3: y -= 1; break;
+            default: throw invalid_argument("Geçersiz yön seçimi.");
+        }
+
+        if (x < 0 || y < 0) {
+            throw out_of_range("Koordinatlar negatif olamaz!");
+        }
+
+        cout << "Mevcut konum: " << x << ", " << y << endl;
+    }
+    catch (const exception &e) {
+        cout << "Hata: " << e.what() << endl;
+    }
+}
+
+void karakter::durumGoster() {
+    cout << "Karakter Durumu:" << endl;
+    cout << "Para: " << para << endl;
+    cout << "Açlık: " << aclik << endl;
+    cout << "Susuzluk: " << susuzluk << endl;
+    cout << "Güç: " << guc << endl;
+}
